6c05d5c6ad91acae0dec409a26f82d9ec5e3c52f
aquietone <quietguy466@gmail.com>
2022-06-04 23:05:43 -0500

    atensilence update

--- lem/events/atensilence.lua
+++ lem/events/atensilence.lua
@@ -1,29 +1,30 @@
 local mq = require('mq')
 
-local my_class = mq.TLO.Me.Class.ShortName()
-
-local pause_cmds = ('/%s mode 0; /mqp on; /twist off; /timed 5 /afollow off; /nav stop; /target clear'):format(my_class)
-local run_away_cmd = '/timed 10 /nav locxyz 1222.67 -48.97 236.41'
-local resume_cmds = ('/timed 150 /%s mode 2; /timed 150 /mqp off; /timed 150 /twist on'):format(my_class)
-
-local full_cmd = ('/multiline ; %s; %s; %s;'):format(pause_cmds, run_away_cmd, resume_cmds)
-
-local function on_load()
-    -- Initialize anything here when the event loads
-end
-
 local function event_handler(line, target)
     if not mq.TLO.Zone.ShortName() == 'vexthaltwo_mission' then return end
 
+    local my_class = mq.TLO.Me.Class.ShortName()
     local i_am_ma = mq.TLO.Group.Member(0).MainAssist()
     local my_name = mq.TLO.Me.CleanName()
     local ma_name = mq.TLO.Group.MainAssist.CleanName()
 
-    if target == my_name and not i_am_ma then
-        mq.cmd(full_cmd)
-    elseif target == ma_name and not i_am_ma then
-        mq.cmd(full_cmd)
+    if not i_am_ma and (target == my_name or target == ma_name) then
+        if my_class == 'BER' and mq.TLO.Me.ActiveDisc.Name() == mq.TLO.Spell('Frenzied Resolve Discipline').RankName() then
+            mq.cmd('/stopdisc')
+        end
+        mq.cmdf('/%s mode 0', my_class)
+        mq.cmd('/mqp on')
+        mq.cmd('/twist off')
+        mq.cmd('/timed 5 /afollow off')
+        mq.cmd('/nav stop')
+        mq.cmd('/target clear')
+        mq.delay(100)
+        mq.cmd('/nav locxyz 1222.67 -48.97 236.41')
+        mq.delay(15000)
+        mq.cmdf('/%s mode 2', my_class)
+        mq.cmd('/mqp off')
+        mq.cmd('/twist on')
     end
 end
 
-return {onload=on_load, eventfunc=event_handler}
+return {eventfunc=event_handler}
